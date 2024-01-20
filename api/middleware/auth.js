a354700542007406ef028fa3d88c32bd229916bb
Danilo D'Alessio <danilo.dalessio@gmail.com>
2024-01-20 16:23:04 +0100

    change in auth token

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -13,7 +13,7 @@ exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
         );
     }
     if (authHeader) {
-        const token = authHeader.split("=")[1];
+        const token = authHeader.split(" ")[1].split("=")[1];
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = await User.findById(decoded.id);
     }
