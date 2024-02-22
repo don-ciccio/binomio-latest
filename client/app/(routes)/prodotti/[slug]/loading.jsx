942af015c602890458e90df3a6e828d72e7d1f11
Danilo D'Alessio <danilo.dalessio@gmail.com>
2024-02-23 00:43:01 +0100

    remove initial data on loading.jsx

--- client/app/(routes)/prodotti/[slug]/loading.jsx
+++ client/app/(routes)/prodotti/[slug]/loading.jsx
@@ -1,19 +1,13 @@
 import Header from "@/app/components/header/Header";
 import SVGLogo from "@/app/components/icons/SVGLogo";
-import { getCategories, getContent } from "@/app/lib/api";
 
 import Breadcrumb from "@/app/components/ui/Breadcrumb";
 import ProductSkeleton from "@/app/components/ui/ProductSkeleton";
 
 export default async function Loading() {
-    const initialCategories = await getCategories("");
-    const initialContent = await getContent();
-
     return (
         <div className='pb-24'>
             <Header
-                categories={initialCategories}
-                message={initialContent.data?.content.topbar}
                 className={`origin-[center_center_0px] align-middle w-20 h-20 block rounded-full`}
             >
                 <SVGLogo className='overflow-hidden align-middle h-full w-full' />
