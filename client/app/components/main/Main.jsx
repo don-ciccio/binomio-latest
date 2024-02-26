9d2f467eea9bf0343e14547643ab72866b59a9c8
Danilo D'Alessio <danilo.dalessio@gmail.com>
2024-02-26 18:09:30 +0100

    fix select time

--- client/app/components/main/Main.jsx
+++ client/app/components/main/Main.jsx
@@ -3,6 +3,7 @@ import React, { useState, useRef, useEffect } from "react";
 import { useGesture } from "@use-gesture/react";
 import { a, useSpring } from "@react-spring/web";
 import CustomDatePicker from "@/app/components/ui/CustomDatePicker";
+import { format } from "date-fns";
 
 /* utils */
 import { vectorScaling, clamp } from "@/app/lib/utils/utilFuncs";
@@ -22,7 +23,11 @@ import { CustomTimePicker } from "../ui/CustomTimePicker";
 const Main = React.forwardRef((props, ref) => {
     const id = "64787b91837e138ddfed4ed0";
     const [startDate, setStartDate] = useState(new Date());
-    const [time, setTime] = useState();
+    const [time, setTime] = useState(new Date());
+
+    const handleDateChange = (date) => {
+        console.log(date);
+    };
 
     const { data } = useGetContentHero();
     const [search, setSearch] = useState("");
@@ -122,6 +127,18 @@ const Main = React.forwardRef((props, ref) => {
         }
     }, []);
 
+    const renderOP = (b, a) => {
+        let td = [];
+        for (let i = b; i <= a; i += 1800000) {
+            td.push(
+                <option key={i} value={i}>
+                    {format(i, "HH:mm")}
+                </option>
+            );
+        }
+        return td;
+    };
+
     return (
         <>
             <div id='home-top' className='relative h-screen	bg-gray-150'>
@@ -176,9 +193,7 @@ const Main = React.forwardRef((props, ref) => {
                                         day.weekday === startDate.getDay() && (
                                             <div key={id}>
                                                 <CustomTimePicker
-                                                    onChange={(date) =>
-                                                        setTime(date.getTime())
-                                                    }
+                                                    onChange={handleDateChange}
                                                     selected={
                                                         day.startBookingHour
                                                     }
@@ -197,30 +212,44 @@ const Main = React.forwardRef((props, ref) => {
                                 })}
                             </div>
                             <div className='relative z-20 bg-transparent dark:bg-form-input'>
-                                <select className='w-32 cursor-pointer appearance-none outline-none  bg-gray-50 border pl-5 p-3 rounded-3xl border-gray-300 text-gray-900 sm:text-sm  focus:ring-slate-500 focus:border-slate-500 block  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-slate-500 dark:focus:border-slate-500'>
-                                    <option disabled value=''>
-                                        Ospiti
-                                    </option>
-                                </select>
-                                <span className='absolute top-1/2 right-4 z-30 -translate-y-1/2'>
-                                    <svg
-                                        className='fill-current'
-                                        width='24'
-                                        height='24'
-                                        viewBox='0 0 24 24'
-                                        fill='none'
-                                        xmlns='http://www.w3.org/2000/svg'
+                                <div className='relative z-20 bg-transparent dark:bg-form-input'>
+                                    <select
+                                        value={time}
+                                        onChange={(e) =>
+                                            setTime(e.target.value)
+                                        }
+                                        className='w-44 cursor-pointer appearance-none outline-none  bg-gray-50 border pl-5 p-3 rounded-3xl border-gray-300 text-gray-900 sm:text-sm  focus:ring-slate-500 focus:border-slate-500 block  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-slate-500 dark:focus:border-slate-500'
                                     >
-                                        <g opacity='0.8'>
-                                            <path
-                                                fillRule='evenodd'
-                                                clipRule='evenodd'
-                                                d='M5.29289 8.29289C5.68342 7.90237 6.31658 7.90237 6.70711 8.29289L12 13.5858L17.2929 8.29289C17.6834 7.90237 18.3166 7.90237 18.7071 8.29289C19.0976 8.68342 19.0976 9.31658 18.7071 9.70711L12.7071 15.7071C12.3166 16.0976 11.6834 16.0976 11.2929 15.7071L5.29289 9.70711C4.90237 9.31658 4.90237 8.68342 5.29289 8.29289Z'
-                                                fill=''
-                                            ></path>
-                                        </g>
-                                    </svg>
-                                </span>
+                                        {weekDays?.map(
+                                            (day, idx) =>
+                                                day.weekday ===
+                                                    startDate.getDay() &&
+                                                renderOP(
+                                                    day.startBookingHour,
+                                                    day.endBookingHour
+                                                )
+                                        )}
+                                    </select>
+                                    <span className='absolute top-1/2 right-4 z-30 -translate-y-1/2'>
+                                        <svg
+                                            className='fill-current'
+                                            width='24'
+                                            height='24'
+                                            viewBox='0 0 24 24'
+                                            fill='none'
+                                            xmlns='http://www.w3.org/2000/svg'
+                                        >
+                                            <g opacity='0.8'>
+                                                <path
+                                                    fillRule='evenodd'
+                                                    clipRule='evenodd'
+                                                    d='M5.29289 8.29289C5.68342 7.90237 6.31658 7.90237 6.70711 8.29289L12 13.5858L17.2929 8.29289C17.6834 7.90237 18.3166 7.90237 18.7071 8.29289C19.0976 8.68342 19.0976 9.31658 18.7071 9.70711L12.7071 15.7071C12.3166 16.0976 11.6834 16.0976 11.2929 15.7071L5.29289 9.70711C4.90237 9.31658 4.90237 8.68342 5.29289 8.29289Z'
+                                                    fill=''
+                                                ></path>
+                                            </g>
+                                        </svg>
+                                    </span>
+                                </div>
                             </div>
                             <button
                                 type='button'
