da99fb70f8e9c0e930d35ee9afb226a2b8db6fee
Danilo D'Alessio <danilo.dalessio@gmail.com>
2024-03-02 13:46:42 +0100

    fix layout with no reservations

--- admin/src/components/bookingTable/BookingTable.jsx
+++ admin/src/components/bookingTable/BookingTable.jsx
@@ -12,13 +12,12 @@ import { format, parseISO } from "date-fns";
 
 const BookingTable = ({ id, date }) => {
     const { data: reservations, isLoading } = useGetReservations({ id, date });
-    if (isLoading) return <div>Loading...</div>;
-
+    console.log(reservations);
     return (
         <>
             {isLoading ? (
                 <TableLoader />
-            ) : (
+            ) : reservations.data.length > 0 ? (
                 <Table className='mt-6'>
                     <TableHead>
                         <TableRow>
@@ -66,6 +65,12 @@ const BookingTable = ({ id, date }) => {
                         )}
                     </TableBody>
                 </Table>
+            ) : (
+                <div className='flex items-center justify-center mt-8 '>
+                    <span className='text-[#6B7280] font-semibold'>
+                        Non ci sono prenotazioni per oggi.
+                    </span>
+                </div>
             )}
         </>
     );
