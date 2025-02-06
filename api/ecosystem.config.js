66f7f00231cea1fcf93757b6a92a94dafc8229e6
Danilo D'Alessio <danilo.dalessio@gmail.com>
2025-02-06 16:07:15 +0100

    change ecosystem

--- api/ecosystem.config.js
+++ api/ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
             name: "binomio-latest",
             script: "npm",
             args: "run dev",
+            interpreter: "/bin/bash",
             env: {
                 NODE_ENV: "development",
                 ENV_VAR1: "environment-variable",
