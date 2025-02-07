f89c02a219e64ac67a06600a38488cc48ae523ff
Danilo D'Alessio <danilo.dalessio@gmail.com>
2025-02-07 15:03:28 +0100

    on push subfolder

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -5,30 +5,19 @@ const ErrorHandler = require("../utils/errorHandler");
 
 // Check if user is authenticated or not
 exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
-    const getAuthToken = (req, res, next) => {
-        if (
-            req.headers.authorization &&
-            req.headers.authorization.split(" ")[0] === "Bearer"
-        ) {
-            req.authToken = req.headers.authorization.split(" ")[1];
-        } else {
-            req.authToken = null;
-        }
-        next();
-    };
+    const token = req.cookies.token;
 
-    try {
-        const token = getAuthToken(req);
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-        req.user = await User.findById(decoded.id);
-
-        next();
-    } catch (e) {
-        return res
-            .status(401)
-            .send({ error: "You are not authorized to make this request" });
+    if (!token) {
+        return next(
+            new ErrorHandler("Login first to access this resource", 401)
+        );
     }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = await User.findById(decoded.id);
+
+    next();
 });
 
 // Handle the user role change
